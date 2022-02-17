0eb11fa4c5852d6af3c4e7069db841ddcbc9cee3
Pekka Luostarinen <75019159+pelu28@users.noreply.github.com>
2022-02-17 12:29:16 +0200

    Add files via upload

---
+++ install.ps1
@@ -0,0 +1,5 @@
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Install Software
+choco install googlechrome notepadplusplus vscode -y
